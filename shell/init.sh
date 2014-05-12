562961de432959ee6c02218165a7324c5fe6af1d
Stefan Krenz <stefan.krenz@mayflower.de>
2014-05-12 16:00:46 +0200

    Improved provisioning

--- shell/init.sh
+++ shell/init.sh
@@ -9,7 +9,7 @@ echo "Installing missing packages..."
 for i in $_PACKAGES; do
     echo $_INSTALLED_PACKAGES|grep "$i" >/dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo " ... $i"
+        echo " - $i"
         yum install -y -q $i 2>/dev/null || exit $?
     fi
 done
@@ -17,7 +17,7 @@ done
 echo "Stopping services..."
 for i in $_SERVICES; do
     service $i status >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
+    if [ $? -eq 0 ]; theni
         service $i stop || exit $?
     fi
 done
@@ -36,8 +36,15 @@ iptables -P OUTPUT ACCEPT || exit $?
 echo "Removing LDAP configuration..."
 rm -rf /etc/openldap/slapd.d/ || exit $?
 
-echo "Copying LDAP configuration..."
-cp -r /vagrant/files/etc/openldap/slapd.d/ /etc/openldap/ || exit $?
+if [ -d /vagrant/files/etc/openldap/slapd.d/ ]; then
+    echo "Copying LDAP configuration..."
+    cp -r /vagrant/files/etc/openldap/slapd.d/ /etc/openldap/ || exit $?
+fi
+if [ -f /vagrant/files/etc/openldap/slapd.d.tgz ]; then
+    echo "Extracting LDAP configuration..."
+    cd /etc/openldap/
+    tar xf /vagrant/files/etc/openldap/slapd.d.tgz || exit $?
+fi
 
 echo "Setting owner for LDAP config directory..."
 chown -R ldap:ldap /etc/openldap/slapd.d/ || exit $?
