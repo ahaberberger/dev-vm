ca5e01ddb1d035f2e388b38faf7158182c234dcf
Stefan Krenz <stefan.krenz@mayflower.de>
2014-05-13 14:14:00 +0200

    Disable SELinux. Added php_excel extension.

--- shell/init.sh
+++ shell/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 _PACKAGES="openldap-servers openldap-clients lsof"
 _SERVICES="slapd httpd"
+_SELINUX_CONFIG="/etc/selinux/config"
 
 echo "Fetching list of installed packages..."
 _INSTALLED_PACKAGES=`yum list installed`
@@ -17,10 +18,18 @@ done
 echo "Stopping services..."
 for i in $_SERVICES; do
     service $i status >/dev/null 2>&1
-    if [ $? -eq 0 ]; theni
+    if [ $? -eq 0 ]; then
         service $i stop || exit $?
     fi
 done
+
+grep 'SELINUX=disabled' $_SELINUX_CONFIG >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+   echo "Disabling SELinux..."
+   cp $_SELINUX_CONFIG ${_SELINUX_CONFIG}.old || exit $?
+   sed -i 's/SELINUX=\([e,p].*\)/SELINUX=disabled/g' $_SELINUX_CONFIG || exit $?
+   echo "SELinux disabled. In order for the changes to take effect, the computer MUST be restarted."
+fi
 
 echo "Stopping firewall and allowing everyone..."
 iptables -F || exit $?
@@ -40,6 +49,7 @@ if [ -d /vagrant/files/etc/openldap/slapd.d/ ]; then
     echo "Copying LDAP configuration..."
     cp -r /vagrant/files/etc/openldap/slapd.d/ /etc/openldap/ || exit $?
 fi
+
 if [ -f /vagrant/files/etc/openldap/slapd.d.tgz ]; then
     echo "Extracting LDAP configuration..."
     cd /etc/openldap/
@@ -51,6 +61,22 @@ chown -R ldap:ldap /etc/openldap/slapd.d/ || exit $?
 
 echo "Removing LDAP data..."
 rm -rf /var/lib/ldap/* || exit $?
+
+_LIBXL_SO="/usr/lib64/libxl.so"
+if [ ! -f $_LIBXL_SO ]; then
+    echo "Installing libxl..."
+    wget -q -O $_LIBXL_SO http://lxwb.wb.mayflower.de/~stefan.krenz/php_excel/oel6.5-x86_64/libxl.so || exit $?
+    chmod +x $_LIBXL_SO
+fi
+
+_PHP_EXCEL_INI="/etc/php.d/excel.ini"
+_PHP_EXCEL_SO="/usr/lib64/php/modules/excel.so"
+if [ ! -f $_PHP_EXCEL_SO ]; then
+    echo "Installing PHP_Excel extension"
+    wget -q -O $_PHP_EXCEL_SO http://lxwb.wb.mayflower.de/~stefan.krenz/php_excel/oel6.5-x86_64/excel.so || exit $?
+    chmod +x $_PHP_EXCEL_SO
+    echo "extension=$_PHP_EXCEL_SO" > $_PHP_EXCEL_INI
+fi
 
 echo "Activating and starting services..."
 for i in $_SERVICES; do
